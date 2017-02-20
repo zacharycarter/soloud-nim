dc613bb1d630f7b8a7082977ece453fe7c071f02
Zachary Carter <carterza@gmail.com>
2017-02-20 11:19:57 -0500

    initial commit,

---
+++ examples/example.nim
@@ -0,0 +1,26 @@
+import ../soloud-nim/soloud, times, os
+
+var i, spin = 0
+
+var sl : ptr Soloud
+
+sl = Soloud_create()
+
+discard Soloud_init(sl)
+
+Soloud_setGlobalVolume(sl, 1)
+
+
+var stream = WavStream_create()
+discard WavStream_load(cast[ptr Wav](stream), "examples/test.ogg")
+
+let currentTime = epochTime()
+let length = WavStream_getLength(stream)
+discard Soloud_play(cast[ptr Soloud](sl), cast[ptr Wav](stream))
+
+while epochTime() - currentTime <= length:
+  sleep(100)
+
+Soloud_deinit(sl)
+
+Soloud_destroy(sl)
---
+++ soloud_nim.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "Zachary Carter"
+description   = "Bindings to the soloud audio library."
+license       = "MIT"
+
+# Dependencies
+
+requires "nim >= 0.16.1"
+
